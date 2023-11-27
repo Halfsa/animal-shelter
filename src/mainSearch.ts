7cf46f3f48a57354f1b5133e5e47e219bb005b50
Fannichan <elekes.fanni2005@gmail.com>
2023-11-27 17:28:10 +0100

    initial

--- src/mainSearch.ts
+++ src/mainSearch.ts
@@ -8,3 +8,20 @@ import { Animals } from './animals';
 let animals:Animals [] = []
 /**Apinak a linkje */
 const apiLink = "https://retoolapi.dev/FTJzOW/Animals"
+
+/**Api megkapjuk az adatokat és azt [animals] tömbe tesszük*/
+function load(){
+    fetch(apiLink).then(final => {
+        /**Fetch sikerességét viszgálja*/
+        if(!final.ok){
+            throw new Error('Error')
+          }
+          return final.json()
+        }).then(inner => {
+          for(let i = 0; i < 50; i++){
+            new Animals(parseInt(inner[i].id), inner[i].name, inner[i].gender, inner[i].species, inner[i].available, inner[i].dateOfBirth)
+          }
+    })
+}
+
+document.addEventListener('DOMContentLoaded', load)
